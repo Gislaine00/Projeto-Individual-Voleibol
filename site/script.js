de6aaedb12838e28f502c8186dee4536f2504a44
Gislaine00 <gislaine.rodrigues@sptech.school>
2023-11-05 21:47:15 -0300

    adionanco conteudo sobre o quiz e sobre mim

--- site/script.js
+++ site/script.js
@@ -1,3 +1,16 @@
+/* navbar*/
+const desiredElement = document.getElementById('nav') // elemento alvo
+const pixelsAmount = '10' // Quantidade de pixels a contar do TOP até definir a cor
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > pixelsAmount) {
+    desiredElement.classList.add('changeStyle') // adiciona classe "changeColor"
+  } else {
+    desiredElement.classList.remove('changeStyle') // remove classe "changeColor"
+  }
+})
+
+/* carrossel */
 var radio = document.querySelector('.manual-btn')
 var i = 1
 document.getElementById('radio1').checked = true
