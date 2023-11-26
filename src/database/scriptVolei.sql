34144d766f8fa58670a2468cd67bcfb609c635cb
Gislaine00 <gislaine.rodrigues@sptech.school>
2023-11-26 17:08:28 -0300

    arrumando alguns detalhes no quiz e no bd

--- src/database/scriptVolei.sql
+++ src/database/scriptVolei.sql
@@ -7,29 +7,33 @@ CREATE TABLE usuario (
     email VARCHAR(45),
     nome VARCHAR(45),
     senha CHAR(6)
-);
+) auto_increment = 100;
 
 CREATE TABLE nickname (
 	idNickname INT PRIMARY KEY auto_increment,
     nickname VARCHAR(45) UNIQUE,
     fkUsuario INT,
     constraint fkUsuario FOREIGN KEY(fkUsuario) REFERENCES usuario(idUsuario)
-);
+) auto_increment = 100;
+
+
+select * from usuario;
 
 CREATE TABLE quiz (
 	idQuiz INT PRIMARY KEY auto_increment,
-    dataHoraConclusão TIMESTAMP,
-    respostaTentativa VARCHAR(45)
-);
-
-CREATE TABLE tentativa (
-	idTentativa INT,
     fkUsuario INT,
-    fkQuiz INT,
     pontuacaoAcertos INT,
     pontuacaoErros INT,
-    constraint fkUsuarioTentativa FOREIGN KEY(fkUsuario) REFERENCES usuario(idUsuario),
-    constraint fkQuizTentativa FOREIGN KEY(fkQuiz) REFERENCES quiz(idQuiz),
-    constraint pkAssociativa PRIMARY KEY (idTentativa, fkUsuario, fkQuiz)
+    constraint fkUsuarioQuiz FOREIGN KEY(fkUsuario) REFERENCES usuario(idUsuario)
 );
+
+select*from usuario;
+    
+select * from quiz;
+
+select pontuacaoAcertos FROM quiz order by pontuacaoAcertos desc limit 5;
+
+
+
+
 
