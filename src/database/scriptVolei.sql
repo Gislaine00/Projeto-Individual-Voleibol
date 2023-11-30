0f0452760c4d1b1d7ddc3a60ae6fcf0fccdc2b05
Gislaine00 <gislaine.rodrigues@sptech.school>
2023-11-29 22:39:00 -0300

    adicionando alguns inserts para já deixar pronto para apresentação

--- src/database/scriptVolei.sql
+++ src/database/scriptVolei.sql
@@ -9,6 +9,15 @@ CREATE TABLE usuario (
     senha CHAR(6)
 ) auto_increment = 100;
 
+INSERT INTO usuario VALUES
+(null, 'antonia@gmail.com', 'Antonia', '123456'),
+(null,'luan@gmail.com', 'Luan', '123456'),
+(null,'ronaldo@gmail.com', 'Ronaldo', '000111'),
+(null,'gislaine@gmail.com', 'Gislaine', '121314'),
+(null,'izadora@gmail.com', 'Izadora', '010203');
+
+select * from usuario;
+
 CREATE TABLE nickname (
 	idNickname INT PRIMARY KEY auto_increment,
     nickname VARCHAR(45) UNIQUE,
@@ -16,8 +25,14 @@ CREATE TABLE nickname (
     constraint fkUsuario FOREIGN KEY(fkUsuario) REFERENCES usuario(idUsuario)
 ) auto_increment = 100;
 
+INSERT INTO nickname VALUES
+(null,'Maria', 100),
+(null,'Lu', 101),
+(null,'Ro', 102),
+(null,'Gis', 103),
+(null,'Iza', 104);
 
-select * from usuario;
+select * from nickname;
 
 CREATE TABLE quiz (
 	idQuiz INT PRIMARY KEY auto_increment,
@@ -27,11 +42,15 @@ CREATE TABLE quiz (
     constraint fkUsuarioQuiz FOREIGN KEY(fkUsuario) REFERENCES usuario(idUsuario)
 );
 
-select*from usuario;
-    
+INSERT INTO quiz VALUES
+(null,100, 10, 0),
+(null,101, 10, 0),
+(null,102, 8, 2),
+(null,103, 7, 3),
+(null,104, 5, 5);
+
 select * from quiz;
 
-select pontuacaoAcertos FROM quiz order by pontuacaoAcertos desc limit 5;
 
 
 
